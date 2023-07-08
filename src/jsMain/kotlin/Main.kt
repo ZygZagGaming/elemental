91c54b9db112c0a0cd2a5d6e31847e2ab28690aa
ZygZagGaming <46930418+ZygZagGaming@users.noreply.github.com>
2023-07-08 00:19:22 -0400

    v1.1.2

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -16,7 +16,7 @@ lateinit var gameState: GameState
 
 var reactionListScrollAmount = 0.0
 var reactionListScrollSens = 0.4
-const val gameVersion = "v1.1.1"
+const val gameVersion = "v1.1.2"
 
 @OptIn(ExperimentalJsExport::class)
 @JsExport
