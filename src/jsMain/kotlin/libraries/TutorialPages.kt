6ccd9da5935811cc5e2ae35f64eb82e1e0949261
ZygZagGaming <46930418+ZygZagGaming@users.noreply.github.com>
2023-11-22 12:13:49 -0500

    moved stuff around and added title screen

--- src/jsMain/kotlin/libraries/TutorialPages.kt
+++ src/jsMain/kotlin/libraries/TutorialPages.kt
@@ -9,7 +9,7 @@ object TutorialPages: Library<TutorialPage>() {
     val titleScreenPage = register(
         "titleScreenPage",
         ImageTitleTutorialPage(
-            image = "images/goober.png",
+            image = "images/tutorial/title_screen.png",
             imageAlt = "goober",
             titleText = "Elemental",
             subTitleText = "Made by ZygZag"
@@ -18,7 +18,7 @@ object TutorialPages: Library<TutorialPage>() {
     val elementsPage = register(
         "elementsPage",
         ImageTextTutorialPage(
-            image = "images/elements.png",
+            image = "images/tutorial/elements.png",
             imageAlt = "elements",
             headerText = "Elements",
             text = "These are your Elements. Most Elements have a bubble showing the Element's symbol and count. You can right-click on Element bubbles to show additional information like bounds and rates."
@@ -27,7 +27,7 @@ object TutorialPages: Library<TutorialPage>() {
     val reactionsPage = register(
         "reactionsPage",
         ImageTextTutorialPage(
-            image = "images/normalreactions.png",
+            image = "images/tutorial/normalreactions.png",
             imageAlt = "reactions",
             headerText = "Reactions",
             text = "These are your Normal Reactions. Each one shows the Elements they consume on the left of the arrow, and the Elements they produce on the right. Normal Reactions can be used as many times as you can afford."
@@ -36,7 +36,7 @@ object TutorialPages: Library<TutorialPage>() {
     val specialReactionsPage = register(
         "specialReactionsPage",
         ImageTextTutorialPage(
-            image = "images/specialreactions.png",
+            image = "images/tutorial/specialreactions.png",
             imageAlt = "special reactions",
             headerText = "Special Reactions",
             text = "These are your Special Reactions. They consume and produce Elements like Normal Reactions, but also have an alternative effect. Special Reactions' cost and effect usually increase each time they are used."
@@ -45,7 +45,7 @@ object TutorialPages: Library<TutorialPage>() {
     val heatPage = register(
         "heatPage",
         ImageTextTutorialPage(
-            image = "images/heat.png",
+            image = "images/tutorial/heat.png",
             imageAlt = "heat",
             headerText = "Heat",
             text = "Heat (h) is an Element that is shown visually around the ϟ bubble. Unlike other elements, heat has a maximum of 10h. You cannot use reactions that would take you over the maximum."
@@ -54,7 +54,7 @@ object TutorialPages: Library<TutorialPage>() {
     val clockStartedHeatPage = register(
         "clockStartedHeatPage",
         ImageTextTutorialPage(
-            image = "images/heat.png",
+            image = "images/tutorial/heat.png",
             imageAlt = "heat",
             headerText = "Heat Dissipation",
             text = "Heat will now dissipate over time. It dissipates faster the more you have."
@@ -63,7 +63,7 @@ object TutorialPages: Library<TutorialPage>() {
     val clickerIntroPage = register(
         "clickerIntroPage",
         ImageTextTutorialPage(
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = "clickers",
             headerText = "Clickers",
             text = "Clickers can be dragged around and when their key is pressed, they will click on the button they're placed on. You can click on a spot in the clicker dock to summon its clicker."
@@ -72,7 +72,7 @@ object TutorialPages: Library<TutorialPage>() {
     val clickerTweakPage = register(
         "clickerTweakPage",
         ImageTextTutorialPage(
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = "clickerTweaking",
             headerText = "Clicker Tweaking",
             text = "Each Clicker can be right-clicked to bring up a menu where you can change the key it listens to, and which mode it's on. Currently, you only have Manual and Disabled modes unlocked."
@@ -81,7 +81,7 @@ object TutorialPages: Library<TutorialPage>() {
     val superclickerPage = register(
         "superclickerPage",
         ImageTextTutorialPage(
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = "superclicker",
             headerText = "Visual Click Speed",
             text = "Once a clicker's cps surpasses 7, it will visually show click speed equal to cps / (7^floor(log_7(cps))). The real click speed is unchanged and can be viewed in the right-click menu."
@@ -90,7 +90,7 @@ object TutorialPages: Library<TutorialPage>() {
     val alteredReactionsPage = register(
         "alteredReactionsPage",
         ImageTextTutorialPage(
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = "altered reactions",
             headerText = "Altered Reactions",
             text = "Altered Reactions are, well, altered reactions. They replace a former reaction, and their name always ends in an asterisk (*)."
@@ -99,7 +99,7 @@ object TutorialPages: Library<TutorialPage>() {
     val deltaReactionRespecPage = register(
         "deltaReactionRespecPage",
         ImageTextTutorialPage(
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = "delta reaction respec",
             headerText = "Delta Reaction Respec",
             text = "With the Duality Milestone Respect unlocked, you can check the \"Respec\" checkbox under the Duality button, and on your next Duality, all of your Duality Reactions will be refunded at no additional cost."
@@ -148,7 +148,7 @@ object TutorialPages: Library<TutorialPage>() {
         ImageTextTutorialPage(
             headerText = "Welcome to Duality!",
             text = "You just carried out a Duality for the first time. Well done!",
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = ""
         )
     )
@@ -159,7 +159,7 @@ object TutorialPages: Library<TutorialPage>() {
             text = "Duality is now always available after reaching 1000000${Symbols.catalyst}. Upon Duality, everything on the Elements " +
                     "page is reset. You will receive one ${Symbols.alpha} for each Element that was at its upper bound, and one ${Symbols.omega} for each " +
                     "Element at its lower bound.",
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = ""
         )
     )
@@ -169,7 +169,7 @@ object TutorialPages: Library<TutorialPage>() {
             headerText = "Duality Milestones",
             text = "These are Duality Milestones. Each one requires a certain quantity of ${Symbols.alpha} and/or ${Symbols.omega} to activate, " +
                     "but ${Symbols.alpha} and ${Symbols.omega} are not consumed.",
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = ""
         )
     )
@@ -179,7 +179,7 @@ object TutorialPages: Library<TutorialPage>() {
             headerText = "Delta Elements",
             text = "These are Delta Elements. Delta Elements are rewarded for reaching a new highest rate of gain for any basic Element. They can be spent " +
                     "on Delta Reactions.",
-            image = "images/goober.png",
+            image = "images/tutorial/goober.png",
             imageAlt = ""
         )
     )
